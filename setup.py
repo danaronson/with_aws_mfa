65efc8ee20bd9c9ad07c5e5c58c4c0216c9ccbad
Dan Aronson <dan.aronson@gmail.com>
2018-06-12 10:51:18 -0700

    removed setting of path

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
  
 setup(
      name='with-aws-mfa',    # This is the name of your PyPI-package.
-     version='0.1',                          # Update the version number for new releases
+     version='0.2',                          # Update the version number for new releases
      scripts=['with-aws-mfa'],                  # The name of your scipt, and also the command you'll be using for calling it
      author='Dan Aronson',
      author_email='dan.aronson@gmail.com',
